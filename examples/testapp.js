2be96c2c599209642a66d4d03aa873dd96aa024a
44gatti <44gatti@gmail.com>
2011-03-25 14:49:26 +0100

    modified:   examples/testapp.js

--- examples/testapp.js
+++ examples/testapp.js
@@ -30,7 +30,7 @@ var log = console.log,
     receivedFiles,
     removedFiles,
     receivedFields,
-    dir =  '/tmp/';
+    dir =  '/var/www/demo/upload/';//'/tmp/';
     
 var handleFormRequest = function(req,res,next){
     receivedFiles = {};
