36ea9363d38577333bcec3f86c8e0325f74f2c34
44gatti <44gatti@gmail.com>
2011-04-30 23:57:05 +0200

    Added client code new file:   examples/client/loadForm.js new file:   examples/client/sendFile.js

---
+++ examples/client/loadForm.js
@@ -0,0 +1,118 @@
+onload = function(){
+
+            function size(bytes){   // simple function to show a friendly size
+                var i = 0;
+                while(1023 < bytes){
+                    bytes /= 1024;
+                    ++i;
+                };
+                return  i ? bytes.toFixed(2) + ["", " Kb", " Mb", " Gb", " Tb"][i] : bytes + " bytes";
+            };
+
+            // create elements
+            var field = document.body.appendChild(document.createElement("input")),
+                br = document.body.appendChild(document.createElement("br")),
+                br1 = document.body.appendChild(document.createElement("br")),
+                input = document.body.appendChild(document.createElement("input")),
+                br2 = document.body.appendChild(document.createElement("br")),
+                br3 = document.body.appendChild(document.createElement("br")),                 
+                bar = document.body.appendChild(document.createElement("div")).appendChild(document.createElement("span")),
+                div = document.body.appendChild(document.createElement("div"));
+            
+            field.setAttribute( "type", "text" );
+            field.setAttribute( "name", "myfield" );
+            field.setAttribute( "name", "myfield" );                        
+            // set input type as file
+            input.setAttribute("type", "file");
+            
+            // enable multiple selection (note: it does not work with direct input.multiple = true assignment)
+            input.setAttribute("multiple", "true");
+            
+            // auto upload on files change
+            input.addEventListener("change", function(){
+                
+                // disable the input
+                input.setAttribute("disabled", "true");
+                
+                sendMultipleFiles({
+                
+                    url: '/test/upload',
+                    // list of files to upload
+                    files:input.files,
+                    
+                    // clear the container 
+                    onloadstart:function(){
+                        div.innerHTML = "Init upload ... ";
+                        bar.style.width = "0px";
+                    },
+                    
+                    // do something during upload ...
+                    onprogress:function(rpe, xhr){ //chunk
+                        //console.log(this.file.fileName);
+                        div.innerHTML = [
+                        " ",
+                            "Uploading: " + this.file.fileName,
+                            "Sent: " + size(rpe.loaded) + " of " + size(rpe.total)                            
+                            //"Total Sent: " + size(this.sent + rpe.loaded) + " of " + size(this.total)
+                        ].join("<br />");
+                        bar.style.width = ( rpe.loaded * 200 / rpe.total >> 0) + "px";//(((this.sent + rpe.loaded) * 200 / this.total) >> 0) + "px";
+                    },
+                    onfileload:function(rpe, xhr, file, end){
+                        //console.log('rpe:',rpe);
+                        //console.log('xhr:',xhr);
+                        //console.log('file:',file);
+                        
+                        bar.style.width = 200;//(((this.sent + rpe.loaded) * 200 / this.total) >> 0) + "px";
+                        var bdy = document.getElementsByTagName("body")[0], // body element
+                            newDiv = document.createElement("div"),
+                            newDiv2 = document.createElement("div"),
+                            newBarContainer = document.createElement("div");
+                            //.appendChild(document.createElement("span"));
+                            newBarContainer.className = 'progress';
+                        var newBar = newBarContainer.appendChild(document.createElement("span"));
+                        
+                        div.innerHTML = [
+                            "Uploading: " + file.fileName,
+                            "Sent: " + size(rpe.loaded) + " of " + size(rpe.total),
+                            "----------------------------------------"
+                        ].join("<br/>");
+                        
+                        if(end){
+                            div.appendChild(newDiv);
+                            newDiv.innerHTML = [
+                                "<br/> Server Response: " + xhr.responseText.replace( /\n/g, '<br/>' ),
+                                "----------------------------------------"
+                            ].join("<br />")+"<br />";
+                            newDiv2.innerHTML = "<br/>**************************<br/> Total Sent: " + size(this.sent + rpe.loaded) + " of " + size(this.total)+"<br/>**************************<br/>";
+                            bdy.appendChild(newDiv2);
+                        }else{
+                          bdy.appendChild(newBarContainer);
+                          div.appendChild(newDiv);
+                          newDiv.innerHTML = [
+                              "<br/> Server Response: " + xhr.responseText.replace( /\n/g, '<br/>' ),
+                              "----------------------------------------"
+                          ].join("<br />")+"<br />";
+                          bdy.appendChild(newDiv2);
+                          div = newDiv2;
+                          bar = newBar;
+                        }
+                        
+                    },
+                    // fired when last file has been uploaded
+                    onload:function(rpe, xhr){
+                        //bar.style.width = "200px";
+                        // enable the input again
+                        //input.removeAttribute("disabled");
+                    },
+                    
+                    // if something is wrong ... (from native instance or because of size)
+                    onerror:function(){
+                        div.innerHTML = "The file " + this.file.fileName + " is too big [" + size(this.file.fileSize) + "]";
+                        // enable the input again
+                        //input.removeAttribute("disabled");
+                    }
+                });
+            }, false);
+            
+            bar.parentNode.className = "progress";
+};
---
+++ examples/client/sendFile.js
@@ -0,0 +1,93 @@
+/** Basic upload manager for single or multiple files (Safari 4 Compatible)
+ * @author  Andrea Giammarchi
+ * @blog    WebReflection [webreflection.blogspot.com]
+ * @license Mit Style License
+ */
+
+var sendFile = 1024*1024*1024; // maximum allowed file size
+                        // should be smaller or equal to the size accepted in the server for each file
+
+// function to upload a single file via handler
+sendFile = (function(toString, maxSize){
+    var isFunction = function(Function){return  toString.call(Function) === "[object Function]";},
+        split = "onabort.onerror.onloadstart.onprogress".split("."),
+        len = split.length;
+    return function(handler){
+        if(maxSize && maxSize < handler.file.fileSize){
+            if(isFunction(handler.onerror))
+                handler.onerror();
+            return;
+        };
+        var xhr = new XMLHttpRequest,
+            upload = xhr.upload;
+            //console.log(handler);
+        for(var xhr = new XMLHttpRequest, upload = xhr.upload, i = 0; i < len; i++ ){
+            upload[split[i]] = ( function(event){
+                return function(rpe){
+                    if(isFunction(handler[event]))
+                        handler[event].call(handler, rpe, xhr);
+                };
+            })(split[i]);
+        }
+        upload.onload = function(rpe){
+            if(handler.onreadystatechange === false){
+                if(isFunction(handler.onload)){
+                    handler.onload(rpe, xhr);
+                }
+            } else {
+                setTimeout(function(){
+                    if(xhr.readyState === 4){
+                        if(isFunction(handler.onload)){
+                            handler.onload(rpe, xhr);
+                        }
+                    } else
+                        setTimeout(arguments.callee, 15);
+                }, 15);
+            }
+        };
+        //console.log(handler.file);
+        xhr.open("post", handler.url || "?upload=true", true);
+        xhr.setRequestHeader("If-Modified-Since", "Mon, 26 Jul 1997 05:00:00 GMT");
+        xhr.setRequestHeader("Cache-Control", "no-cache");
+        xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        xhr.setRequestHeader("X-File-Name", handler.file.fileName);
+        xhr.setRequestHeader("X-File-Size", handler.file.fileSize);
+        xhr.setRequestHeader("X-File-Type", handler.file.type);        
+        xhr.setRequestHeader("Content-Type", "multipart/form-data");
+        xhr.send(handler.file);
+        return  handler;
+    };
+})(Object.prototype.toString, sendFile);
+
+// function to upload multiple files via handler
+function sendMultipleFiles(handler){
+    var len = handler.files.length,
+        i = 0,
+        //onFileLoad = handler.onFileLoad,
+        onload = handler.onload;
+
+    handler.current = 0;
+    handler.total = 0;
+    handler.sent = 0;
+    while(handler.current < len)
+        handler.total += handler.files[handler.current++].fileSize;
+    handler.current = 0;
+    if(len){
+        handler.file = handler.files[handler.current];
+        sendFile(handler).onload = function(rpe, xhr){
+            if(++handler.current < len){
+                handler.sent += handler.files[handler.current - 1].fileSize;
+                handler.file = handler.files[handler.current];
+                sendFile(handler).onload = arguments.callee;
+                handler.onfileload(rpe, xhr, handler.files[handler.current - 1],false);
+                //handler.onload(rpe, xhr);
+            } else if(onload) {
+                handler.onload = onload;
+                handler.onfileload(rpe,xhr,handler.file,len,true);
+                handler.onload(rpe, xhr);
+            }
+        };
+    };
+    return  handler;
+};
+
