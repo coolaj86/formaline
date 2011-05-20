6e58dca3fa706e716477e6c6ee1d13ee2cb694dd
44gatti <44gatti@gmail.com>
2011-05-20 14:39:37 +0200

    Modified dataProgress in getProgressEmitter modified:   examples/simpleUpload.js modified:   lib/formaline.js

--- examples/simpleUpload.js
+++ examples/simpleUpload.js
@@ -68,7 +68,7 @@ var handleFormRequest = function( req, res, next ){
             // default is false, or integer chunk factor, 
             // every n chunk emit event 1+(0*n) 1+(1*n),1+(2*n),1+(3*n), 
             // minimum factor value is 2 -->
-        emitDataProgress: false,//false,true,3,10,100
+        emitDataProgress: !false,//false,true,3,10,100
         
             // max bytes allowed, this is the max bytes written to disk before stop to write 
             // this is also true for serialzed fields not only for files upload  -->
