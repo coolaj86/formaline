97bc666c6dd1b90f227006568c859ff925108117
44gatti <44gatti@gmail.com>
2011-05-24 04:16:37 +0200

    modified:   examples/simple/simpleUpload.js modified:   lib/formaline.js

--- examples/simple/simpleUpload.js
+++ examples/simple/simpleUpload.js
@@ -94,7 +94,7 @@ var http = require( 'http' ),
                     // enable various logging levels
                     // it is possible to switch on/off one or more levels at the same time
                     // debug: 'off' turn off logging
-                logging: 'debug:on,1:on,2:off,3:off',
+                logging: 'debug:on,1:off,2:off,3:off',
                 
                     // listeners
                 listeners: {
