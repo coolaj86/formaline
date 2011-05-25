1ae4afb08462435c34777e48fc9691302db3062f
44gatti <44gatti@gmail.com>
2011-05-25 15:41:58 +0200

    modified:   examples/simple/simpleUpload.js modified:   lib/formaline.js

--- examples/simple/simpleUpload.js
+++ examples/simple/simpleUpload.js
@@ -62,8 +62,7 @@ var http = require( 'http' ),
                     // the returned session id param, must contain a String, not a function or an object 
                     // the function takes http request as a parameter at run-time 
                 getSessionID: function( req ){ 
-                    return 'ciao'
-                    //return ( ( req.sessionID ) || ( req.sid ) || ( ( req.session && req.session.id ) ? req.session.id : null ) );
+                    return ( ( req.sessionID ) || ( req.sid ) || ( ( req.session && req.session.id ) ? req.session.id : null ) );
                 },
   
                     // default is false
